2763da1701e9758c964648084c3ceb69587ec6bf
Sreenath Ottapurakkal Kallada <oksreenath@gmail.com>
2021-03-12 23:13:12 +0100

    Second commit

--- redis1.py
+++ redis1.py
@@ -6,7 +6,7 @@ redis_port = 6379
 redis_password = ""
 
 def hello_redis():
-    """Example Hello Redis Program"""
+    """Example Hello Redis Progra"""
     try:
         r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
         r.set('ip_address', '0.0.0.0')
